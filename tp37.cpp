11e030ceb2c28aec9316c9d6432a54ca7d4df6cc
malesal <133693731+malesal@users.noreply.github.com>
2023-05-21 20:08:36 -0300

    Add files via upload

---
+++ tp37.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int CalcularMaximo(int a, int b);
+
+int main() {
+    int num1, num2, maximo;
+    
+    cout << "Ingrese dos numeros enteros: ";
+    cin >> num1 >> num2;
+    maximo = CalcularMaximo(num1, num2);
+    
+    cout << "El maximo entre " << num1 << " y " << num2 << " es: " << maximo << endl;
+    
+    return 0;
+}
+
+int CalcularMaximo(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
